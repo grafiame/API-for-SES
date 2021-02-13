1f7245ba829e0cab8d3df472263abd2d93c08aa3
Gabriel Monteiro <sm.gabriel007@gmail.com>
2021-02-12 22:14:31 -0300

    Added authentication

--- src/server.js
+++ src/server.js
@@ -9,6 +9,20 @@ setQueues([new BullAdapter(Queue.queues.map(queue => queue.bull)[0])])
 setQueues([new BullAdapter(Queue.queues.map(queue => queue.bull)[1])])
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith('Bearer ')
+  )
+    return res.sendStatus(401)
+  const idToken = req.headers.authorization.split('Bearer ')[1]
+  if(idToken == process.env.EMAIL_API_TOKEN) {
+    return next()
+  }
+  return res.sendStatus(401)
+});
+
 app.post('/sendemail', UserController.store)
 
 app.use('/admin/queues', router)
