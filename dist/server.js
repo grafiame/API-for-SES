88bc21d11c9db5928a3d448ebf71de99f2b4f90d
Gabriel Monteiro <sm.gabriel007@gmail.com>
2021-02-15 12:56:39 -0300

    Build after implementing authentication

--- dist/server.js
+++ dist/server.js
@@ -16,6 +16,16 @@ const app = (0, _express.default)();
 (0, _bullBoard.setQueues)([new _bullBoard.BullAdapter(_Queue.default.queues.map(queue => queue.bull)[0])]);
 (0, _bullBoard.setQueues)([new _bullBoard.BullAdapter(_Queue.default.queues.map(queue => queue.bull)[1])]);
 app.use(_express.default.json());
+app.use((req, res, next) => {
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) return res.sendStatus(401);
+  const idToken = req.headers.authorization.split('Bearer ')[1];
+
+  if (idToken == process.env.EMAIL_API_TOKEN) {
+    return next();
+  }
+
+  return res.sendStatus(401);
+});
 app.post('/sendemail', _UserController.default.store);
 app.use('/admin/queues', _bullBoard.router);
 app.listen(process.env.SERVER_PORT, () => {
